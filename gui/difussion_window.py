91497bd1648b0f57915d620926d29370292df807
Jdparra2004 <jdparra2004@gmail.com>
2024-11-06 19:52:25 -0500

    Update Simulation
    Ajuste Simulación en difusión

--- gui/difussion_window.py
+++ gui/difussion_window.py
@@ -1,6 +1,8 @@
 #librerias gui
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
 from PyQt5 import uic
+from simulations.combined_simulations import CombinedSimulation
+from simulations.visualization_3d import generate_3d_image
 
 class DifussionWindow(QMainWindow):
     def __init__(self, main_window):
@@ -13,8 +15,43 @@ class DifussionWindow(QMainWindow):
         self.buttonStopSimulation.clicked.connect(self.StopSimulation)
         self.returnToMenu.clicked.connect(self.VolverMenu)
         
+        # Inicialización de la simulación
+        self.simulation = None
+        
+        # Configurar área para la imagen 3D
+        self.setup_3d_view()
+    
+    def setup_3d_view(self):
+        # Configurar el área donde se mostrará la imagen 3D
+        self.imageLabel = QLabel(self)
+        self.imageLabel.setGeometry(50, 50, 400, 400) # ajustar al tamaño de la interfaz
+        
+        
     def RunSimulation(self):
-        pass
+        # Obtener parámetros de la interfaz
+        params = self.get_parameters()
+        
+        # Iniciar simulación
+        self.simulation = CombinedSimulation(params)
+        
+        # Instancia Difusión
+        if isinstance(self, DifussionWindow):
+            data = self.simulation.get_simulation_data(method="fd")
+            image_path = generate_3d_image(data, type = "concentration")
+            
+        # Mostrar imagen
+        sefl.display_3d_image(image_path)
+    
+    def get_parameters(self):
+        return {
+            'Ro'
+            'Ri'
+        }
+    
+    def display_3d_image(self, image_path):
+        pixmap = QPixmap(image_path)
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setScaledContents(True)
     
     def StopSimulation(self):
         pass
