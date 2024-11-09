8beea7eff8b194ab5800045758a20ae450d469cb
Jdparra2004 <jdparra2004@gmail.com>
2024-11-08 21:12:13 -0500

    New scripts in the project

---
+++ gui/comparison_window.py
@@ -0,0 +1,24 @@
+#librerias gui
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
+from PyQt5 import uic
+
+class ComparisonWindow(QMainWindow):
+    def __init__(self, main_window):
+        super().__init__()
+        uic.loadUi('resources/comparison_window.ui', self)
+        self.main_window = main_window
+        
+        #botones
+        self.buttonDifFinitas.clicked.connect(self.DifFinitas)
+        self.buttonHomotopia.clicked.connect(self.Homotopia)
+        self.returnToMenu.clicked.connect(self.VolverMenu)
+        
+    def DifFinitas(self):
+        pass
+    
+    def Homotopia(self):
+        pass
+    
+    def VolverMenu(self):
+        self.close()
+        self.main_window.show()
---
+++ gui/tangential_stress_window.py
@@ -0,0 +1,24 @@
+#librerias gui
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
+from PyQt5 import uic
+
+class TangentialStressWindow(QMainWindow):
+    def __init__(self, main_window):
+        super().__init__()
+        uic.loadUi('resources/tangential_stress_window.ui', self)
+        self.main_window = main_window
+        
+        #botones
+        self.buttonRunSimulation.clicked.connect(self.RunSimulation)
+        self.buttonStopSimulation.clicked.connect(self.StopSimulation)
+        self.returnToMenu.clicked.connect(self.VolverMenu)
+        
+    def RunSimulation(self):
+        pass
+    
+    def StopSimulation(self):
+        pass
+    
+    def VolverMenu(self):
+        self.close()
+        self.main_window.show()
